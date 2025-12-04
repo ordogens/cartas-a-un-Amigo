a19a38f26f8048e1e8f00b3a12068329f889c539
OrichalcosShunnoros <c3146092091@gmail.com>
2025-12-04 01:25:15 -0500

    Creada carpeta data para albergar archivos de datos (antes, array)

---
+++ src/data/jean.ts
@@ -0,0 +1,20 @@
+interface Datos  {
+    id: number;
+    fecha: string;
+    titulo: string;
+    carta: string;
+}
+
+export const data: Datos [] = [
+
+    {id: 1, titulo:"repuestas", fecha:"28/11/205", carta: 'As de corazones'},
+    {id: 2, titulo:"repuestas", fecha:"28/11/205", carta: 'As de corazones'},
+    {id: 3, titulo:"repuestas", fecha:"28/11/205", carta: 'As de corazones'},
+    {id: 4, titulo:"repuestas", fecha:"28/11/205", carta: 'As de corazones'},
+
+
+
+]
+   
+
+
